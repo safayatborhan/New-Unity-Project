befcc67f8c60e982534ab61473bc0145616a23f3
Safayat Zisan <zisan_cse_mbstu@hotmail.com>
2016-11-18 18:44:22 +0600

    Add project files.

---
+++ Assets/SharpVariables.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SharpVariables : MonoBehaviour {
+
+	// Use this for initialization
+
+	int a=5;
+	int b=7;
+	int sum,sub;
+	void Start () {
+		sum = a + b;
+		sub = b - a;
+		Debug.Log (sum);
+		Debug.Log (sub);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
